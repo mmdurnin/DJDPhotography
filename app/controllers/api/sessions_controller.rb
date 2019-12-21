de8df555f13ae8c8ae59a392b9adba6768cbc27b
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-21 10:55:41 -0800

    backend user auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,18 @@
 class Api::SessionsController < ApplicationController
+
+    def create
+        if (params[:username] == ENV['USERNAME'] && params[:password] == ENV['PASSWORD'])
+            ENV['SESSION_TOKEN'] = SecureRandom::urlsafe_base64
+            session[:session_token] = ENV['SESSION_TOKEN']
+        else
+            render json: ["That's not the right password. Your computer will self destruct in 3.. 2.."]
+            ENV['SESSION_TOKEN'] = ""
+            session[:session_token] = ""
+        end
+    end 
+
+    def destroy
+        ENV['SESSION_TOKEN'] = ""
+        session[:session_token] = ""
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :logged_in?
+
+    def logged_in? 
+        !!ENV["SESSION_TOKEN"]
+    end
+
+    def require_login  
+        redirect_to :root unless logged_in?
+    end
+
+
 end
