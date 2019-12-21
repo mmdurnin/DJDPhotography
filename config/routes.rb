0458b0d6c01f813b8ef3b4e9bda0ba9e2409d501
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-20 18:26:05 -0800

    add jquery-rails gem and set environment variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
 
-    resource :session, only:[:create, :destroy, :show]
+    resource :session, only:[:create, :destroy, :new]
     resources :photos
   end
 
