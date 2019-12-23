db6fce0d890517840bad4b4d9663be6fb6735eaf
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-23 01:36:34 -0800

    style photos page

--- frontend/components/photos.jsx
+++ frontend/components/photos.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+
 class Photos extends React.Component {
     constructor(props) {
         super(props)
     }
 
     render() {
-        return(
-            <div>Photos page</div>
-        )
+
+        const images = []
+        for (let i = 1; i < 13; i++) {
+            images.push(
+              <div className="img-index-item">
+                <img
+                  src={window.imgArray[i]}
+                  key={i}>
+                </img>
+              </div>
+            );
+        }
+
+        return (
+          <div className="image-index">
+            {images}
+          </div>
+        );
     }
 }
 
@@ -17,7 +33,7 @@ const msp = (state) => {
     console.log(state)
     // photos: state.photos
     return {
-        
+
     }
 }
 
