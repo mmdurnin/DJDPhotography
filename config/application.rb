42cb6c944180457e4347785471b09bd7fbedd4da
Maureen Durnin <maureenmdurnin@gmail.com>
2019-12-25 02:17:06 -0800

    test

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,11 @@ module DJDPhotography
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
